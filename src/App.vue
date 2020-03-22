68a719a7e88c1581dac3749e764a6aa506beb8b1
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-03-22 11:34:25 +0800

    todo app using vuex core

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>{{title}}</h2>
+    <form @submit.prevent="addTodo">
+      <label for="newTodo">New Todo</label>
+      <input type="text" id="newTodo" name="newTodo" v-model="newTodo">
+      <button type="submit" name="button">Add</button>
+    </form>
+    <button @click="allDone" type="button" name="button">Add Done</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        <input @click="toggleTodoDone(index)" type="checkbox" :checked="todo.done">
+        <span :class="{done: todo.done}">{{todo.title}}</span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { mapState, mapActions, mapMutations } from 'vuex';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  computed: {
+    newTodo: {
+      get() {
+        return this.$store.state.newTodo;
+      },
+      set(value) {
+        this.$store.commit('setNewTodo', value);
+      },
+    },
+    ...mapState(['todos', 'title']),
+  },
+  methods: {
+    ...mapMutations(['allDone']),
+    ...mapActions(['addTodo', 'toggleTodoDone']),
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    margin-top: 60px
+  }
+  .done {
+    text-decoration: line-through;
+  }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,62 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    title: 'Introduction to Vuex',
+    newTodo: '',
+    todos: [
+      {
+        id: 1,
+        title: 'Learn Vuex.JS',
+        done: false,
+      },
+      {
+        id: 2,
+        title: 'Learn ReactJS',
+        done: true,
+      },
+    ],
   },
   mutations: {
+    setNewTodo(state, value) {
+      state.newTodo = value;
+    },
+
+    addTodo({ todos, newTodo }) {
+      todos.push({
+        title: newTodo,
+        done: false,
+      });
+    },
+
+    setTodoDone(state, index) {
+      state.todos[index].done = true;
+    },
+
+    setTodoNotDone(state, index) {
+      state.todos[index].done = false;
+    },
+
+    allDone(state) {
+      state.todos.forEach((todo) => {
+        // eslint-disable-next-line no-param-reassign
+        todo.done = true;
+      });
+    },
   },
   actions: {
+    addTodo({ commit }) {
+      commit('addTodo');
+      commit('setNewTodo', '');
+    },
+
+    toggleTodoDone(context, index) {
+      const todo = context.state.todos[index];
+      if (todo.done) {
+        context.commit('setTodoNotDone', index);
+      } else {
+        context.commit('setTodoDone', index);
+      }
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
